abfa292c1bbe010728eebf99d11965ddc7819226
Nizar Fergag <fergag99@gmail.com>
2022-03-24 10:30:06 +0100

    checkare i box

--- asset/js/script.js
+++ asset/js/script.js
@@ -6,4 +6,21 @@ for(i = 1; i <= 100; i++){
     box.innerHTML += i;
     box.classList.add("box");
     grid.appendChild(box);
+
+    // ora voglio che al clic si colori la cella
+
+    box.addEventListener("click", function(){
+        this.classList.add("clicked");
+    })
 }
+
+// const btn = document.getElementById("btn");
+// const level = document.getElementById("level");
+
+// btn.onclick = (event) => {
+//     console.log(level);
+// }
+
+// if (level == "lev-1"){
+//     console.log(level)
+// }
