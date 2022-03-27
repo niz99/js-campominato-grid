de5de7be7862eba46416b70afe2ea00584f214f5
Nizar Fergag <fergag99@gmail.com>
2022-03-27 15:35:57 +0200

    esercizio completato

--- asset/js/script.js
+++ asset/js/script.js
@@ -1,26 +1,80 @@
+let start = document.getElementById("btn");
+
+let select = document.getElementById("level");
+
 let grid = document.getElementById("grid");
 
-for(i = 1; i <= 100; i++){
+let paragrafo = document.getElementById("welcome");
 
-    let box = document.createElement("div");
-    box.innerHTML += i;
-    box.classList.add("box");
-    grid.appendChild(box);
+let array = [];
 
-    // ora voglio che al clic si colori la cella
+start.addEventListener("click", function(){
 
-    box.addEventListener("click", function(){
-        this.classList.add("clicked");
-    })
-}
+    grid.innerHTML = "";
 
-// const btn = document.getElementById("btn");
-// const level = document.getElementById("level");
+    grid.classList.remove("none");
 
-// btn.onclick = (event) => {
-//     console.log(level);
+    paragrafo.classList.add("none");
+
+    console.log(select.value);
+
+    if(select.value == "lev-1"){
+
+        for(i = 1; i <= 100; i++){
+
+            let box = document.createElement("div");
+            box.innerHTML += i;
+            box.classList.add("box-facile");
+            grid.appendChild(box);
+
+            box.addEventListener("click", function(){
+                this.classList.add("clicked");
+            })
+        }
+
+    } else if(select.value == "lev-2"){
+
+        for(i = 1; i <= 81; i++){
+
+            let box = document.createElement("div");
+            box.innerHTML += i;
+            box.classList.add("box-medio");
+            grid.appendChild(box);
+
+            box.addEventListener("click", function(){
+                this.classList.add("clicked");
+            })
+        }
+
+    } else {
+
+        for(i = 1; i <= 49; i++){
+
+            let box = document.createElement("div");
+            box.innerHTML += i;
+            box.classList.add("box-difficile");
+            grid.appendChild(box);
+
+            box.addEventListener("click", function(){
+                this.classList.add("clicked");
+            })
+        }
+
+    }
+})
+
+// function getRandomIntInclusive(min, max){
+//     min = Math.ceil(min);
+//     max = Math.ceil(max);
+//     return Math.floor(Math.random() * (max - min + 1) + min);
 // }
 
-// if (level == "lev-1"){
-//     console.log(level)
+// for(y = 1; y <= 100; i++){
+
+//     let numeroRandom = getRandomIntInclusive(1, 100);
+
+//     do {
+//         array,push(numeroRandom)
+//     } while ( !array.includes(numeroRandom) )
+
 // }
